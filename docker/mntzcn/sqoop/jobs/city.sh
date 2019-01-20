e4c0637e7d3a1fc64f90d73ad5e4dcf5b712ad73
miguelhalysortuno <miguel.halys.contractor@bbva.com>
2019-01-20 22:26:09 +0100

    feature: now running fact and dimension ingestion is done with docker-compose

--- docker/mntzcn/sqoop/jobs/city.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-MYSQL_TABLE=city
-
-sqoop job --create $MYSQL_TABLE -- import --connect $MYSQL_HOST$MYSQL_DB --username $MYSQL_USER --password-file $MYSQL_PASS --table $MYSQL_TABLE --warehouse-dir $MNTZCN_APPLICATION_HDFS_PATH --m 1
-sqoop job --exec $MYSQL_TABLE
--- docker/mntzcn/sqoop/jobs/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# https://stackoverflow.com/questions/496702/can-a-shell-script-set-environment-variables-of-the-calling-shell/28489593#28489593
-SQOOP_JOBS_HOME=/opt/sqoop/jobs
-. $SQOOP_JOBS_HOME/env.sh
-. $SQOOP_JOBS_HOME/antenna.sh
-. $SQOOP_JOBS_HOME/city.sh
-. $SQOOP_JOBS_HOME/client.sh
