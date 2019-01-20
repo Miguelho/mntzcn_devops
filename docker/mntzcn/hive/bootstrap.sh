020caa6a69fd72a60d11ee0907829ddf548f67c3
miguelhalysortuno <miguel.halys.contractor@bbva.com>
2019-01-20 22:04:30 +0100

    refactor

--- docker/mntzcn/hive/bootstrap.sh
+++ docker/mntzcn/hive/bootstrap.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
 sed s/HOSTNAME/hadoop/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
-
-/opt/hive/bin/hive -f /opt/hive/ddl.hql
 
 tail -f /dev/null
